c0e2bf32d8f85a51e0c60523c03a4488c257c45b
Voragun Supawong <voragun2543@gmail.com>
2022-08-19 20:41:26 +0700

    find max value by return value in binary search tree

--- code/Start.java
+++ code/Start.java
@@ -1,19 +1,36 @@
-
-import javax.swing.plaf.RootPaneUI;
-
 class Start {
     public static void main(String ... data) {
-        int[] a = { 3, 5, 7, 6, 2, 4, 1 };
-        Element root = Tree.create(a);
-        System.out.println(root.value);
+        int[] a = { 3, 5, 7, 6, 9, 2, 4, 1 };
+        java.util.Arrays.sort(a);
+        Element root = Tree.create(a);  // This is root node
+        // System.out.println(root.value);
         
         // Write code to find the maximum value in the given binary tree
-        int m = Tree.findMax(root);
-        System.out.println(m);
+        // int m = Tree.findMax(root);
+        // System.out.println(m);
+        
+        // Write code to find the maximum value in the given binary search tree
+        // using non-recursive
+        Element e = root;
+        for ( ; e.right != null; e = e.right) {
+            
+        }
+        System.out.println(e.value);
+        
+        // print everything in the right path
+        // for (Element e = root; e != null; e = e.right) {
+        //    System.out.println(e.value); // 4 6 7
+        // }
+        
     }
 }
 
 class Tree {
+    
+    static int findMaxBinarySearchTree (Element current) {
+        
+        return 0;
+    }
     
     static int findMax(Element current) {
         if (current == null) return 0;
