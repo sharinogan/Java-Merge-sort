b60b3521fb77d20cfd19235c8ce809998b199850
Voragun Supawong <voragun2543@gmail.com>
2022-08-19 21:32:13 +0700

    Star1

--- code/Start.java
+++ code/Start.java
@@ -2,11 +2,29 @@ class Start {
     public static void main(String ... data) {
         int[] a = { 8, 5, 4, 2, 7, 1, 9, 3 };
         Node head = Link.create(a);
+        Node second = Link.cutMiddle(head);
+        System.out.println("First : ");
         Link.print(head);
+        System.out.println("Second : ");
+        Link.print(second);
     }
 }
 
 class Link {
+    
+    static Node cutMiddle(Node start) {
+        if(start == null) return null;            // No data
+        if(start.next == null) return null;       // One Node
+        
+        Node rabbit = start, turtle = start;
+        while(rabbit != null && rabbit.next != null && rabbit.next.next != null) {
+            rabbit = rabbit.next.next;
+            turtle = turtle.next;
+        }
+        Node result = turtle.next;
+        turtle.next = null;        // null
+        return result;
+    }
     
     // Linked list
     static Node create(int[] data) {
